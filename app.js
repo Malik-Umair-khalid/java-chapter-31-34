e680c13aa7780cd2dba65d9fb670fd527f5e2c4b
Malik Umair Khalid <fighteromega3462@gmail.com>
2021-05-31 12:21:16 +0500

    Assugnment Completed

---
+++ app.js
@@ -0,0 +1,126 @@
+// *** Q 1 *** //
+
+// var currentDate = new Date () 
+// alert(currentDate)
+
+// *** Q 2 *** //
+
+// var currentMonth = new Date () .getMonth()
+// // alert(currentMonth)
+// var months = ["Janvary","Ferervary","March","April","May","June","Jully","August","Novenber","October","September","Decenber",]
+// alert(months[currentMonth])
+
+// *** Q 3 *** //
+
+// var currentdate = new Date ()
+// var datetostring = currentdate.toString()
+// var currentDay = datetostring.slice(0,3)
+// alert(currentDay)
+
+// *** Q 4 *** //
+
+// var currentdate = new Date ()
+// var datetostring = currentdate.toString()
+// var currentDay = datetostring.slice(0,3)
+
+// if (currentDay === "Sat" || currentDay === "Sun"){
+//     alert("It is a funny day")
+// }
+// else{
+//     alert("It is not a funny day")
+// }
+
+// *** Q 5 *** //
+
+// var date = new Date ().getDate
+// if(date <= 15){
+//     alert("First Fifteen Days")
+// }
+// else{
+//     alert("Last Fifteen Days")
+// }
+
+// *** Q 6 *** //
+
+// var date = new Date () .getTime();
+// var minuts = date / 1000
+// alert(minuts)
+
+// *** Q 7 *** //
+
+// var time = new Date() .getHours()
+// if(time > 12 && time >= 13){
+//     alert("It Is Pm")
+// }
+// else(
+//     alert("It IS Am")
+// )
+// *** Q 8 *** //
+
+// var lastyear = new Date ().setFullYear(2020)
+// alert("Full Year")
+
+// *** Q 10 *** //
+
+// var firstRamadanDate = new Date ("June 18, 2015").getTime()
+// var todayDate = new Date () . getTime()
+// var difference =  todayDate - firstRamadanDate 
+// var days = difference/ (1000 * 60 * 60 * 24);
+// console.log(days)
+
+
+// *** Q 11 *** //
+
+
+// var date2015 = new Date ("Sat Dec 05 2015 22:50:16 GMT+0500 (PKT)").getTime()
+// // console.log(date2015)
+// var dateToday = new Date().getTime()
+// var milliSeconds = dateToday - date2015
+// var minutes = milliSeconds / 1000
+// console.log(minutes)
+
+// *** Q 12 *** //
+
+// var date = new Date () 
+// var dateHour = Date(-6106015800000)
+// alert(dateHour)
+
+// *** Q 13 *** //
+
+// var d = new Date();
+// d.setFullYear(1991);
+// console.log(d)
+
+// *** Q 14 *** //
+
+// var dob = new Date(prompt("Enter Your Birh day")); 
+// var month_diff = Date.now() - dob.getTime();  
+// var age_dt = new Date(month_diff);      
+// var year = age_dt.getUTCFullYear();  
+// var age = Math.abs(year - 1970);  
+// document.write("Age of the date entered: " + age + " years");
+
+// *** Q 15 *** //
+
+// var costumerName = "Umair"
+// var months = ["Janvary","Ferervary","March","April","May","June","Jully","August","Novenber","October","September","Decenber",]
+// var currentMonth = new Date ().getMonth()
+// var numberOFUnits = 5000
+// var chargerPerUnit = 5
+// var lateCharges = 8
+// var a = months[currentMonth]
+// var b = months[currentMonth - 1]
+// document.write('<h3>' + costumerName + '</h3>')
+// document.write('<h3>' + "Current Month" + a + '</h3>')
+// document.write('<h3>' + numberOFUnits + '</h3>')
+// document.write('<h3>' + "Later Month" + b + '</h3>')
+// document.write('<h3>' + chargerPerUnit + '</h3>')
+// document.write('<h3>' + "Late ChargerPerUnit" + lateCharges + '</h3>')
+// document.write('<h3>' + "Payable Within Due Date" + chargerPerUnit * numberOFUnits + '</h3>')
+// document.write('<h3>' + "Late ChargerPerUnit" + lateCharges * numberOFUnits + '</h3>')
+
+// *** Q 1 *** //
+
+// *** Q 1 *** //
+
+// *** Q 1 *** //
